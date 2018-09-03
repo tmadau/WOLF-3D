cacf2f6270b082d0e3720b1bc807949afc77945e
TEEKAY <takimadau@gmail.com>
2018-09-03 16:41:50 +0200

    make now working with sdl

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tmadau <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/05/24 14:02:48 by tmadau            #+#    #+#              #
-#    Updated: 2018/08/20 08:59:27 by tmadau           ###   ########.fr        #
+#    Updated: 2018/09/03 16:39:30 by tmadau           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,29 +14,29 @@ NAME = wolf3d
 
 KGRN = \x1B[32m
 
-CFILES = test.c \
-
-HF = libft.h \
+CFILES = check.c \
 
 LIB = ./libft/libft.a
 
 INCLUDES = -I./libft
 
-OFILES = $(CFILES:.c=.o)
-
 CC = gcc
 
 CF = -c -Wall -Wextra -Werror
+
+SDLFLAGS = -lSDL2 -I ~/.brew/include -L ~/.brew/lib
+
+OFILES = $(CFILES:.c=.o)
 
 all: $(NAME)
 
 $(NAME): $(OFILES)
 	@echo "$(KGRN)#*******************************************************#"
-	@echo "$(KGRN)# WOLF3D LOADING █ ██ ███ ████ █████ ██████ ███████████ #"
+	@echo "$(KGRN)# WOLF3D LOADING █ ██ ███████ ███████████ █████████████ #"
 	@echo "$(KGRN)#*******************************************************#"
 	@make -C libft/
 	@$(CC) $(CF) -I $(LIB) -c $(CFILES)
-	@$(CC) -o $(NAME) $(OFILES) -lm -L libft/ -lft -lmlx -framework OpenGL -framework AppKit
+	@$(CC) -o $(NAME) $(OFILES) $(SDLFLAGS)
 	@echo "$(KGRN)#*******************************************************#"
 	@echo "$(KGRN)#             WOLF3D READY [ ✔ ][ ✔ ][ ✔ ]              #"
 	@echo "$(KGRN)#*******************************************************#"
--- wolf3d.h
+++ wolf3d.h
@@ -1,64 +1,91 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmadau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/09 12:22:41 by tmadau            #+#    #+#             */
-/*   Updated: 2018/08/21 10:54:55 by tmadau           ###   ########.fr       */
+/*   Created: 2018/09/03 13:01:22 by tmadau            #+#    #+#             */
+/*   Updated: 2018/09/03 16:18:29 by tmadau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WOLF3D_H
-# define WOLF3D_H
+#ifndef WOLF_H
+# define WOLF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# include <stdlib.h>
+//# include <SDL2/SDL.h>
+# include "/goinfre/tmadau/.brew/include/SDL2/SDL.h"
 
-# include "gfx.h"
+# define WIN_HEIGHT 350
+# define WIN_WIDTH 515
+# define BUFF_SIZE	32
 
-typedef struct	s_mlx
+typedef struct		s_info
 {
-	void		*mlx;
-	void		*win;
-	int			i;
-}				t_mlx;
+	double			**map;
+	int				row;
+	int				x;
+	int				y;
+	int				fill_x;
+	int				fill_y;
+	int				fd;
+	int				running;
+	SDL_Window		*window;
+	SDL_Renderer	*renderer;
+	SDL_Event		event;
+	int				height;
+	int				width;
+	double			pos_x;
+	double			pos_y;
+	double			dir_x;
+	double			dir_y;
+	double			plane_x;
+	double			plane_y;
+	double			time;
+	double			old_time;
+	double			count_x;
+	double			cam_x;
+	double			ray_pos_x;
+	double			ray_pos_y;
+	double			ray_dir_x;
+	double			ray_dir_y;
+	int				map_x;
+	int				map_y;
+	double			delta_dist_x;
+	double			delta_dist_y;
+	int				hit;
+	int				step_x;
+	int				step_y;
+	double			side_dist_x;
+	double			side_dist_y;
+	int				side;
+	double			perp_wall_dist;
+	int				line_height;
+	int				draw_start;
+	int				draw_end;
+	int				count_j;
+	int				count_y;
+	double			frame_time;
+	double			move_speed;
+	double			rot_speed;
+	double			old_dir_x;
+	double			old_plane_x;
+}					t_info;
 
-typedef struct	s_dda
-{
-	int			x1;
-	int			y1;
-	int			x2;
-	int			y2;
-	int			z1;
-	int			z2;
-	double		scale;
-}				t_dda;
-
-typedef	struct	s_inc
-{
-	int			i;
-	int			steps;
-	double		x_inc;
-	double		y_inc;
-	double		z_inc;
-}				t_inc;
-
-typedef	struct	s_pixel
-{
-	int			dif_x;
-	int			dif_y;
-	int			dif_z;
-	float		x;
-	float		y;
-	float		z;
-	int			steps;
-}				t_pixel;
-
-typedef struct	s_sum
-{
-	char		***val;
-	double		z_axis;
-	int			x;
-	int			y;
-}				t_sum;
+int					get_next_line(const int fd, char **line);
+void				get_x_and_y(char *str, t_info *ev);
+int					ft_count(const char *s, char c);
+void				map_alloc(char *str, t_info *ev);
+t_info				ft_ray_calc(t_info *ev);
+t_info				ft_side_dist_calc(t_info *ev);
+t_info				ft_dda_calc(t_info *ev);
+t_info				ft_draw_stuff(t_info *ev);
+t_info				ft_render_stuff(t_info *ev);
+t_info				draw_ceiling(t_info *ev);
+void				events(t_info *ev);
 
 #endif
